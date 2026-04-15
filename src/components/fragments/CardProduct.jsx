eadf3fc7d24dc455f2c5f4daa1d478ff1a6dd77c
mulzahrian <mulzahrian@aprilasia.com>
2026-04-15 16:59:35 +0700

    add:rendering list

--- src/components/fragments/CardProduct.jsx
+++ src/components/fragments/CardProduct.jsx
@@ -3,18 +3,19 @@ import Button from "../elements/Button";
 const CardProduct =(props) => {
     const {children} = props;
     return(
-       <div className="w-full max-w-sm bg-purple-300 border border-purple-700 rounded-lg shadow-lg">
+       <div className="w-full max-w-sm bg-purple-300 border border-purple-700 rounded-lg shadow-lg mx-2 flex flex-col justify-between">
         {children}
       </div>
     );
 
 }
 
-const Header = () => {
+const Header = (props) => {
+    const { imageSrc } = props;
     return(
         <a href="#">
           <img
-            src="/images/shoes-1.jpg"
+            src={imageSrc}
             alt="Product Image"
             className="p-8 rounded-t-lg"
           />
@@ -22,25 +23,27 @@ const Header = () => {
     );
 }
 
-const Body = () => {
+const Body = (props) => {
+    const { title, children } = props;
     return(
-       <div className="px-5 pb-5">
+       <div className="px-5 pb-5 h-full">
           <a href="#">
             <h5 className="text-xl font-semibold tracking-light text-center text-white">
-              New Shoes
+              {title}
             </h5>
             <p className="text-white text-sm mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque animi non quod deserunt.
+              {children}
             </p>
           </a>
         </div> 
     );
 }
 
-const Footer = () => {
+const Footer = (props) => {
+    const { price } = props;
     return(
         <div className="flex items-center justify-between px-5 pb-5">
-          <span className="text-3xl font-bold text-white">$299</span>
+          <span className="text-3xl font-bold text-white">${price}</span>
           <Button className="bg-black text-purple-600 hover:bg-gray-200 font-bold py-2 px-4 rounded">
             Add to Cart
           </Button>
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,14 +1,35 @@
 import Button from "../components/elements/Button";
 import CardProduct from "../components/fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    title: "New Shoes",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque animi non quod deserunt.",
+    price: 299,
+    imageSrc: "/images/shoes-1.jpg",
+  },
+  {
+    id: 2,
+    title: "New Shoes",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque animi non quod deserunt.",
+    price: 299,
+    imageSrc: "/images/shoes-1.jpg",
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header />
-        <CardProduct.Body />
-        <CardProduct.Footer />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header imageSrc={product.imageSrc} />
+          <CardProduct.Body title={product.title}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
